169873c8350e89e4fd6ba284e8edef8fa861300b
Minseok Kim <min3482@snu.ac.kr>
2016-01-25 23:50:43 +0900

    user signup login completed, navbar added, admin hw list load completed

--- js/controllers/AdminController.js
+++ js/controllers/AdminController.js
@@ -1,8 +1,9 @@
 app.controller('AdminController', ['$scope', 'storage', '$mdMedia', '$mdDialog', '$http', '$filter', function ($scope, storage, $mdMedia, $mdDialog, $http, $filter) {
 
     //문제목록 불러올 때 필터링 기준 - course 기준
-    $scope.targetCourse = '';
+
     $scope.courseList = ['수학1/2', '미적분1', '미적분2', '확률과통계'];
+    $scope.targetCourse = $scope.courseList[3].trim();
 
     //문제목록 불러올 때 필터링 기준 - date 기준
 
@@ -12,18 +13,32 @@ app.controller('AdminController', ['$scope', 'storage', '$mdMedia', '$mdDialog',
         "date2":new Date()
     };
 
+    $scope.dateList = [];
+    $scope.groupList = [];
+
     $scope.getHwList = function () {
-        var date = {
+        var params = {
             "date1":$filter('date')(new Date($scope.datepicker.date1), 'yyyy-MM-dd'),
-            "date2":$filter('date')(new Date($scope.datepicker.date2), 'yyyy-MM-dd')
+            "date2":$filter('date')(new Date($scope.datepicker.date2), 'yyyy-MM-dd'),
+            "group":$scope.targetCourse.trim()
         };
-        $http.get("http://localhost:12080/api/problems", {params: date})
+        $http.get("http://localhost:12080/api/problems", {params: params})
             .then(function (response) {
                 console.log(response.data.data);
                 console.log($scope.datepicker.date2);
                 console.log(new Date($scope.datepicker.date2));
-                console.log(date);
-                $scope.problemList = response.data.data
+                console.log(params);
+                $scope.problemList = response.data.data;
+                for(var i = 0; i < $scope.problemList.length; i++) {
+                    if($scope.dateList.indexOf($scope.problemList[i].date)<0) {
+                        $scope.dateList.push($scope.problemList[i].date);
+                    }
+                }
+                for(var i = 0; i < $scope.problemList.length; i++) {
+                    if($scope.groupList.indexOf($scope.problemList[i].group.name)<0) {
+                        $scope.groupList.push($scope.problemList[i].group.name);
+                    }
+                }
             });
     };
 
