ffed6841db08e66af0bfc498480fc4fbca34f405
Minseok Kim <min3482@snu.ac.kr>
2016-02-17 00:23:10 +0900

    과제 목록 정렬방식 추가 - 제출완료

--- js/controllers/HwListController.js
+++ js/controllers/HwListController.js
@@ -30,6 +30,22 @@ app.controller('HwListController', ['$scope', 'storage','$mdMedia', '$mdDialog',
         })
     };
 
+    $scope.checkHw = function(ev, homework) {
+        $mdDialog.show({
+            controller: HwContentController,
+            templateUrl: 'templates/hw-content.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen:true,
+            scope: $scope,
+            preserveScope: true,
+            locals: {
+                homework: homework
+            }
+        })
+    };
+
     function HwContentController($scope, $mdDialog, homework) {
         $scope.hide = function() {
             $mdDialog.hide();
@@ -44,11 +60,11 @@ app.controller('HwListController', ['$scope', 'storage','$mdMedia', '$mdDialog',
         $scope.answerOptions = ['맞음', '틀렸는데 고침', '틀렸는데 모름', '모름'];
         $http.get(host + "/homeworks/"+homework.id+"/problems")
             .then(function (response) {
-                $scope.problemList = response.data.data.problems;
-                $scope.homeworkObj = response.data.data.homework;
-                for (var i=0; i < $scope.problemList.length; i++) {
-                    $scope.problemList[i]['answer'] = "";
-                };
+                console.log(response);
+                $scope.problemList = response.data.data;
+                //for (var i=0; i < $scope.problemList.length; i++) {
+                //    $scope.problemList[i]['aaanswer'] = "";
+                //};
                 console.log($scope.problemList);
             });
 
